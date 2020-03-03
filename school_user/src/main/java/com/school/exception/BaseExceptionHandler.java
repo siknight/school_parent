0b727dfc19e67845b56f259f89ff3f126c0fa3a0
lisi <1786678583@qq.com>
2020-03-03 23:01:36 +0800

    用户1

--- school_user/src/main/java/com/school/exception/BaseExceptionHandler.java
+++ school_user/src/main/java/com/school/exception/BaseExceptionHandler.java
@@ -14,7 +14,7 @@ public class BaseExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public Result error(Exception e){
-        e.printStackTrace();
+        e.printStackTrace(); //
         return new Result(false, StatusCode.ERROR, e.getMessage());
     }
 }
